5260092bf34c2943a7acee4d897516c90bb5c2ea
kitpaddle <49337981+kitpaddle@users.noreply.github.com>
2022-07-02 17:32:00 +0200

    Update script.js
    Adding code so it resizes map automatically if window is resized.

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let viewport_height = document.documentElement.clientHeight;
 document.getElementById('main').style.width = viewport_width+'px';
 document.getElementById('main').style.height = viewport_height+'px';
 
+// Add listener to resize when window resizes.
+window.addEventListener('resize', function(event) {
+  viewport_width = document.documentElement.clientWidth;
+  viewport_height = document.documentElement.clientHeight;
+  document.getElementById('main').style.width = viewport_width+'px';
+  document.getElementById('main').style.height = viewport_height+'px';
+  document.getElementById('tooltip').style.height = viewport_height-80+'px';
+}, true);
